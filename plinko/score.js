672e33eb2fd6fd2f6a96fed65e90c504f4b8078d
Miroslav Muras <mirekmuras@gmail.com>
2019-01-14 01:01:30 -0500

    setup

--- plinko/score.js
+++ plinko/score.js
@@ -1,18 +1,52 @@
+
+
 const outputs = [];
-const predictionPoint = 300;
-const k = 3;
+
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   // Ran every time a balls drops into a bucket
   outputs.push([dropPosition, bounciness, size, bucketLabel]);
-
-  //console.log(outputs);
 }
 
 function runAnalysis() {
   // code here to analyze stuff
-   const bucket = _.chain(outputs)
-      .map(row => [distance(row[0]), row[3]])
+  const testSetSize = 50;
+  const [testSet, trainingSet] = splitDataset(minMAX(outputs,3), testSetSize);
+
+ 
+  _.range(1,15).forEach( k => {
+     const accuracy =  _.chain(testSet)
+    .filter(
+      testPoint => knn(trainingSet, _.initial(testPoint), k) === testPoint[3]
+      )
+    .size()
+    .divide(testSetSize)
+    .value();
+
+    console.log('For each k of ',k,' accuracy using KNN algorithm is ', accuracy);
+  });
+
+}
+
+/**********************************************************************************/
+
+// IF Prediction was bad !
+// 1) Adjust the parameters of the analysis
+// 2) Add more features to explain the analysis
+// 3) Change the prediction point
+// 4) Accept that maybe there isn't a good correlation
+
+
+/***********************************************************************************/
+
+function knn (data, point, k) {
+     return  _.chain(data)
+      .map(row => {
+        return [
+          distance(_.initial(row), point), 
+          _.last(row)
+        ];          
+      })
       .sortBy(row => row[0])
       .slice(0, k)
       .countBy(row => row[1])
@@ -24,8 +58,13 @@ function runAnalysis() {
       .value()
 }
 
-function distance(point) {
-  return Math.abs(point - predictionPoint);
+
+function distance(pointA, pointB) {
+  return _.chain (pointA)
+        .zip(pointB)
+        .map(([a, b]) => (a-b) ** 2)
+        .sum()
+        .value() ** 0.5 
 }
 
 // @dev: split dataset into two subsets "training" set & "test" set
@@ -34,13 +73,26 @@ function splitDataset(data, testCount) {
   const shuffled = _.shuffle(data);                   // suffle dataset
 
   const testSet = _.slice(shuffled, 0, testCount);    // test = shuffled data from 0 to testCount
-  const trainSet = _.slice(shuffled, testCount)       // trainSet = shuffled data from testCount to the end
+  const trainingSet = _.slice(shuffled, testCount)       // trainSet = shuffled data from testCount to the end
 
-  return [testSet, trainSet];
+  return [testSet, trainingSet];
+}
 
+function minMAX(data, featureCount) {
+    const clonedData = _.cloneDeep(data);
 
+    // column of an array
+    for(let i = 0; i < featureCount; i++) {
+      const column = clonedData.map(row => row[i]);
+      const min = _.min(column);
+      const max = _.max(column);    
 
-  
+    // row of an array
+      for( let j = 0; j < clonedData.length; j++) {
+        clonedData[j][i] =(clonedData[j][i] - min) / (max - min);
+        }
+    }
 
+    return clonedData;
 }
 
