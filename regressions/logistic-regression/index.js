59c97b4dac4b8ac8a76616ab7cb14967480b2cd4
Miroslav Muras <mirekmuras@gmail.com>
2019-03-24 21:09:59 -0400

    Logistic_regression commit

--- regressions/logistic-regression/index.js
+++ regressions/logistic-regression/index.js
@@ -1,16 +1,32 @@
 require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
 const loadCSV = require('../load-csv');
+const LogisticRegression = require('./logistic-regression');
+const plot = require('node-remote-plot');
 
 const{features, labels, testFeatures, testLabels} = loadCSV('../data/cars.csv', {
-    dataColumns:[
-        'horsepower',
-        'displacment',
-        'weight'
-    ],
-    labelColumns: ['passedmissions'],
-    shuffle: true,
-    splitTest: 50
+    dataColumns:['horsepower','displacement','weight'],
+    labelColumns: ['passedemissions'],
+    shuffle: true,                                  // shuffle all values in the data set
+    splitTest: 10,                                  // 
+    converters: {                                   // convert string value to an integer
+        passedemissions: (value) => {
+            return value === 'TRUE' ? 1 : 0       // from passedmission column , if the value is 'TRUE' return 1, else 0
+            }
+        }
+    }
+);
 
+const regression = new LogisticRegression(features, labels, {
+    learningRate: 0.3,
+    iterations: 100,
+    batchSize: 50,
+});
 
+regression.train();
+//regression.predict([[175, 400, 2.57], [90,140,1.13] , [88,150,1.16] ]).print();
+console.log(regression.test(testFeatures, testLabels));
+
+plot({
+    x: regression.cost_History_Array.reverse()
 });
